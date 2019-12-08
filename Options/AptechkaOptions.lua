c41ada8e68310e6f3367dac0f12e0397a7f806aa
d87 <d87d87@gmail.com>
2019-12-08 21:51:19 +0700

    Option panel tweaks

--- Options/AptechkaOptions.lua
+++ Options/AptechkaOptions.lua
@@ -897,12 +897,190 @@ local function MakeGeneralOptions()
                         order = 3,
                     },
                 },
-            }, --
-            sizeSettings = {
+            },
+            switches = {
                 type = "group",
                 name = " ",
                 guiInline = true,
                 order = 3,
+                args = {
+                    showSolo = {
+                        name = L"Show Solo",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.showSolo end,
+                        set = function(info, v)
+                            Aptechka.db.showSolo = not Aptechka.db.showSolo
+                            Aptechka:ReconfigureProtected()
+                        end,
+                        order = 8,
+                    },
+                    showParty = {
+                        name = L"Show In Party",
+                        type = "toggle",
+                        width = "double",
+                        get = function(info) return Aptechka.db.showParty end,
+                        set = function(info, v)
+                            Aptechka.db.showParty = not Aptechka.db.showParty
+                            Aptechka:ReconfigureProtected()
+                        end,
+                        order = 8.1,
+                    },
+                    RMBClickthrough = {
+                        name = L"RMB Mouselook Clickthrough"..newFeatureIcon,
+                        desc = L"Allows to turn with RMB without moving mouse away from the unitframes.\nIf using Clique, this will override its RMB binding",
+                        type = "toggle",
+                        width = "full",
+                        confirm = true,
+                        confirmText = L"Warning: Requires UI reloading.",
+                        get = function(info) return Aptechka.db.RMBClickthrough end,
+                        set = function(info, v)
+                            Aptechka.db.RMBClickthrough = not Aptechka.db.RMBClickthrough
+                            ReloadUI()
+                        end,
+                        order = 8.3,
+                    },
+                    sortUnitsByRole = {
+                        name = L"Sort Units by Role",
+                        width = "full",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.sortUnitsByRole end,
+                        set = function(info, v)
+                            Aptechka.db.sortUnitsByRole = not Aptechka.db.sortUnitsByRole
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 8.5,
+                    },
+
+                    disableBlizzardParty = {
+                        name = L"Disable Blizzard Party Frames",
+                        width = "double",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.disableBlizzardParty end,
+                        set = function(info, v)
+                            Aptechka.db.disableBlizzardParty = not Aptechka.db.disableBlizzardParty
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 9,
+                    },
+                    hideBlizzardRaid = {
+                        name = L"Hide Blizzard Raid Frames",
+                        width = "full",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.hideBlizzardRaid end,
+                        set = function(info, v)
+                            Aptechka.db.hideBlizzardRaid = not Aptechka.db.hideBlizzardRaid
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 10,
+                    },
+                    -- disableBlizzardRaid = {
+                    --     name = "Disable Blizzard Raid Frames (not recommended)",
+                    --     width = "full",
+                    --     type = "toggle",
+                    --     confirm = true,
+                    -- 	confirmText = "Warning: Will completely disable Blizzard CompactRaidFrames, but you also lose raid leader functionality. If you delete this addon, you can only revert with this macro:\n/script EnableAddOn('Blizzard_CompactRaidFrames'); EnableAddOn('Blizzard_CUFProfiles')",
+                    --     get = function(info) return not IsAddOnLoaded("Blizzard_CompactRaidFrames") end,
+                    --     set = function(info, v)
+                    --         Aptechka:ToggleCompactRaidFrames()
+                    --     end,
+                    --     order = 10.4,
+                    -- },
+                    petGroup = {
+                        name = L"Enable Pet Group",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.petGroup end,
+                        set = function(info, v)
+                            Aptechka.db.petGroup = not Aptechka.db.petGroup
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 15.7,
+                    },
+                    disableTooltip = {
+                        name = L"Disable Tooltips",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.disableTooltip end,
+                        set = function(info, v)
+                            Aptechka.db.disableTooltip = not Aptechka.db.disableTooltip
+                        end,
+                        order = 10.8,
+                    },
+                    showAggro = {
+                        name = L"Show Aggro",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.showAggro end,
+                        set = function(info, v)
+                            Aptechka.db.showAggro = not Aptechka.db.showAggro
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 10.9,
+                    },
+                    showAFK = {
+                        name = L"Show AFK",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.showAFK end,
+                        set = function(info, v)
+                            Aptechka.db.showAFK = not Aptechka.db.showAFK
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 11,
+                    },
+                    showRaidIcons = {
+                        name = L"Show Raid Icons",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.showRaidIcons end,
+                        set = function(info, v)
+                            Aptechka.db.showRaidIcons = not Aptechka.db.showRaidIcons
+                            if not Aptechka.db.showRaidIcons then
+                                Aptechka:ForEachFrame(function(self) self.raidicon:Hide() end)
+                            else
+                                Aptechka:RAID_TARGET_UPDATE()
+                            end
+                        end,
+                        order = 11.1,
+                    },
+                    --[==[
+                    useCLH = {
+                        name = L"Use LibCLH",
+                        disabled = true,
+                        type = "toggle",
+                        confirm = true,
+                        confirmText = L"Warning: Requires UI reloading.",
+                        order = 11.3,
+                        get = function(info) return Aptechka.db.useCombatLogHealthUpdates end,
+                        set = function(info, v)
+                            Aptechka.db.useCombatLogHealthUpdates = not Aptechka.db.useCombatLogHealthUpdates
+                            ReloadUI()
+                        end
+                    },
+                    ]==]
+                    useDebuffOrdering = {
+                        name = L"Use Debuff Ordering",
+                        type = "toggle",
+                        order = 11.2,
+                        get = function(info) return Aptechka.db.useDebuffOrdering end,
+                        set = function(info, v)
+                            Aptechka.db.useDebuffOrdering = not Aptechka.db.useDebuffOrdering
+                            Aptechka:UpdateDebuffScanningMethod()
+                        end
+                    },
+                    showCasts = {
+                        name = L"Show Casts",
+                        type = "toggle",
+                        get = function(info) return Aptechka.db.showCasts end,
+                        set = function(info, v)
+                            Aptechka.db.showCasts = not Aptechka.db.showCasts
+                            Aptechka:PrintReloadUIWarning()
+                        end,
+                        order = 11.4,
+                    },
+                }
+            },
+
+            sizeSettings = {
+                type = "group",
+                name = " ",
+                guiInline = true,
+                order = 4,
                 args = {
                     width = {
                         name = L"Width",
@@ -1003,181 +1181,7 @@ local function MakeGeneralOptions()
                         step = 1,
                         order = 7,
                     },
-                    showSolo = {
-                        name = L"Show Solo",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.showSolo end,
-                        set = function(info, v)
-                            Aptechka.db.showSolo = not Aptechka.db.showSolo
-                            Aptechka:ReconfigureProtected()
-                        end,
-                        order = 8,
-                    },
-                    showParty = {
-                        name = L"Show In Party",
-                        type = "toggle",
-                        width = "double",
-                        get = function(info) return Aptechka.db.showParty end,
-                        set = function(info, v)
-                            Aptechka.db.showParty = not Aptechka.db.showParty
-                            Aptechka:ReconfigureProtected()
-                        end,
-                        order = 8.1,
-                    },
-                    RMBClickthrough = {
-                        name = L"RMB Mouselook Clickthrough"..newFeatureIcon,
-                        desc = L"Allows to turn with RMB without moving mouse away from the unitframes.\nIf using Clique, this will override its RMB binding",
-                        type = "toggle",
-                        width = "full",
-                        confirm = true,
-                        confirmText = L"Warning: Requires UI reloading.",
-                        get = function(info) return Aptechka.db.RMBClickthrough end,
-                        set = function(info, v)
-                            Aptechka.db.RMBClickthrough = not Aptechka.db.RMBClickthrough
-                            ReloadUI()
-                        end,
-                        order = 8.3,
-                    },
-                    sortUnitsByRole = {
-                        name = L"Sort Units by Role",
-                        width = "full",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.sortUnitsByRole end,
-                        set = function(info, v)
-                            Aptechka.db.sortUnitsByRole = not Aptechka.db.sortUnitsByRole
-                            Aptechka:PrintReloadUIWarning()
-                        end,
-                        order = 8.5,
-                    },
 
-                    disableBlizzardParty = {
-                        name = L"Disable Blizzard Party Frames",
-                        width = "double",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.disableBlizzardParty end,
-                        set = function(info, v)
-                            Aptechka.db.disableBlizzardParty = not Aptechka.db.disableBlizzardParty
-                            Aptechka:PrintReloadUIWarning()
-                        end,
-                        order = 9,
-                    },
-                    hideBlizzardRaid = {
-                        name = L"Hide Blizzard Raid Frames",
-                        width = "full",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.hideBlizzardRaid end,
-                        set = function(info, v)
-                            Aptechka.db.hideBlizzardRaid = not Aptechka.db.hideBlizzardRaid
-                            Aptechka:PrintReloadUIWarning()
-                        end,
-                        order = 10,
-                    },
-                    -- disableBlizzardRaid = {
-                    --     name = "Disable Blizzard Raid Frames (not recommended)",
-                    --     width = "full",
-                    --     type = "toggle",
-                    --     confirm = true,
-                    -- 	confirmText = "Warning: Will completely disable Blizzard CompactRaidFrames, but you also lose raid leader functionality. If you delete this addon, you can only revert with this macro:\n/script EnableAddOn('Blizzard_CompactRaidFrames'); EnableAddOn('Blizzard_CUFProfiles')",
-                    --     get = function(info) return not IsAddOnLoaded("Blizzard_CompactRaidFrames") end,
-                    --     set = function(info, v)
-                    --         Aptechka:ToggleCompactRaidFrames()
-                    --     end,
-                    --     order = 10.4,
-                    -- },
-                    petGroup = {
-                        name = L"Enable Pet Group",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.petGroup end,
-                        set = function(info, v)
-                            Aptechka.db.petGroup = not Aptechka.db.petGroup
-                            Aptechka:PrintReloadUIWarning()
-                        end,
-                        order = 10.7,
-                    },
-                    disableTooltip = {
-                        name = L"Disable Tooltips",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.disableTooltip end,
-                        set = function(info, v)
-                            Aptechka.db.disableTooltip = not Aptechka.db.disableTooltip
-                        end,
-                        order = 10.8,
-                    },
-                    showAggro = {
-                        name = L"Show Aggro",
-                        type = "toggle",
-                        confirm = true,
-                        confirmText = L"Warning: Requires UI reloading.",
-                        get = function(info) return Aptechka.db.showAggro end,
-                        set = function(info, v)
-                            Aptechka.db.showAggro = not Aptechka.db.showAggro
-                            ReloadUI()
-                        end,
-                        order = 10.9,
-                    },
-                    showAFK = {
-                        name = L"Show AFK",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.showAFK end,
-                        set = function(info, v)
-                            Aptechka.db.showAFK = not Aptechka.db.showAFK
-                            Aptechka:PrintReloadUIWarning()
-                        end,
-                        order = 11,
-                    },
-                    showRaidIcons = {
-                        name = L"Show Raid Icons",
-                        type = "toggle",
-                        get = function(info) return Aptechka.db.showRaidIcons end,
-                        set = function(info, v)
-                            Aptechka.db.showRaidIcons = not Aptechka.db.showRaidIcons
-                            if not Aptechka.db.showRaidIcons then
-                                Aptechka:ForEachFrame(function(self) self.raidicon:Hide() end)
-                            else
-                                Aptechka:RAID_TARGET_UPDATE()
-                            end
-                        end,
-                        order = 11.1,
-                    },
-                    --[==[
-                    useCLH = {
-                        name = L"Use LibCLH",
-                        disabled = true,
-                        type = "toggle",
-                        confirm = true,
-                        confirmText = L"Warning: Requires UI reloading.",
-                        order = 11.3,
-                        get = function(info) return Aptechka.db.useCombatLogHealthUpdates end,
-                        set = function(info, v)
-                            Aptechka.db.useCombatLogHealthUpdates = not Aptechka.db.useCombatLogHealthUpdates
-                            ReloadUI()
-                        end
-                    },
-                    ]==]
-                    useDebuffOrdering = {
-                        name = L"Use Debuff Ordering",
-                        type = "toggle",
-                        confirm = true,
-                        confirmText = L"Warning: Requires UI reloading.",
-                        order = 11.2,
-                        get = function(info) return Aptechka.db.useDebuffOrdering end,
-                        set = function(info, v)
-                            Aptechka.db.useDebuffOrdering = not Aptechka.db.useDebuffOrdering
-                            ReloadUI()
-                        end
-                    },
-                    showCasts = {
-                        name = L"Show Casts",
-                        type = "toggle",
-                        confirm = true,
-                        confirmText = L"Warning: Requires UI reloading.",
-                        get = function(info) return Aptechka.db.showCasts end,
-                        set = function(info, v)
-                            Aptechka.db.showCasts = not Aptechka.db.showCasts
-                            ReloadUI()
-                        end,
-                        order = 11.4,
-                    },
 
                     orientation = {
                         name = L"Health Orientation",
