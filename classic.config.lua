52f286561f1653d4bdab262b349b23654d57b475
d87 <d87d87@gmail.com>
2020-08-03 23:03:25 +0700

    Shaman config changes to use new BarArray

--- classic.config.lua
+++ classic.config.lua
@@ -13,7 +13,7 @@ local IsPlayerSpell = IsPlayerSpell
 local color1 = { 0.9, 0, 0 }
 local bossDebuff = { type = "HARMFUL", assignto = "bossdebuff", color = color1, priority = 40, pulse = true }
 local tankCD = { type = "HELPFUL", assignto = "icon", global = true, showDuration = true, priority = 94}
-local survivalCD = { type = "HELPFUL", assignto = "shieldicon", global = true, showDuration = true, priority = 90 }
+local survivalCD = { type = "HELPFUL", assignto = "buffIcons", global = true, showDuration = true, priority = 90 }
 
 -- WARLOCK
 A{ id = { 6229, 11739, 11740, 28610 }, prototype = survivalCD } -- Shadow Ward
@@ -180,21 +180,25 @@ if playerClass == "SHAMAN" then
     -- Healing Way
     A{ id = 29203, type = "HELPFUL", assignto = "bar4", showStacks = 3, color = {38/255, 221/255, 163/255} }
 
+    local prioWater = 75
+    local prioAir = 74
+    local prioEarth = 73
+    local prioFire = 72
     -- Earth
-    A{ id = { 8072, 8156, 8157, 10403, 10404, 10405 }, type = "HELPFUL", assignto = "totemCluster2", isMine = true, color = {151/255, 86/255, 168/255} }  -- Stoneskin Totem
-    A{ id = { 8076, 8162, 8163, 10441, 25362 }, type = "HELPFUL", assignto = "totemCluster2", isMine = true, color = {15/255, 78/255, 60/255} }  -- Strength of Earth Totem
+    A{ id = { 8072, 8156, 8157, 10403, 10404, 10405 }, type = "HELPFUL", assignto = "raidbuff", priority = prioEarth, isMine = true, color = {151/255, 86/255, 168/255} }  -- Stoneskin Totem
+    A{ id = { 8076, 8162, 8163, 10441, 25362 }, type = "HELPFUL", assignto = "raidbuff", priority = prioEarth, isMine = true, color = {15/255, 78/255, 60/255} }  -- Strength of Earth Totem
     -- Fire
-    A{ id = { 8182, 10476, 10477 }, type = "HELPFUL", assignto = "raidbuff", isMine = true, color = { 1,0.4,0.4} }  -- Frost Resistance Totem
+    A{ id = { 8182, 10476, 10477 }, type = "HELPFUL", assignto = "raidbuff", priority = prioFire, isMine = true, color = { 1,0.4,0.4} }  -- Frost Resistance Totem
     -- Water
-    A{ id = { 16191, 17355, 17360 }, type = "HELPFUL", assignto = "totemCluster1", isMine = true, color = {38/255, 221/255, 163/255} }  -- Mana Tide Totem
-    A{ id = { 5677, 10491, 10493, 10494 }, type = "HELPFUL", assignto = "totemCluster1", isMine = true, color = { 187/255, 75/255, 128/255 } }  -- Mana Spring Totem
-    A{ id = { 5672, 6371, 6372, 10460, 10461 }, type = "HELPFUL", assignto = "totemCluster1", isMine = true, color = { 0.63, 0.8, 0.35 } }  -- Healing Stream Totem
-    A{ id = { 8185, 10534, 10535 }, type = "HELPFUL", assignto = "totemCluster1", isMine = true, color = { 65/255, 110/255, 1 } }  -- Fire Resistance Totem
+    A{ id = { 16191, 17355, 17360 }, type = "HELPFUL", assignto = "raidbuff", priority = prioWater, isMine = true, color = {38/255, 221/255, 163/255} }  -- Mana Tide Totem
+    A{ id = { 5677, 10491, 10493, 10494 }, type = "HELPFUL", assignto = "raidbuff", priority = prioWater, isMine = true, color = { 187/255, 75/255, 128/255 } }  -- Mana Spring Totem
+    A{ id = { 5672, 6371, 6372, 10460, 10461 }, type = "HELPFUL", assignto = "raidbuff", priority = prioWater, isMine = true, color = { 0.63, 0.8, 0.35 } }  -- Healing Stream Totem
+    A{ id = { 8185, 10534, 10535 }, type = "HELPFUL", assignto = "raidbuff", priority = prioWater, isMine = true, color = { 65/255, 110/255, 1 } }  -- Fire Resistance Totem
     -- Air
-    A{ id = 8178, type = "HELPFUL", assignto = "totemCluster3", isMine = true, color = { 0.6, 0, 1 } }  -- Grounding Totem
-    A{ id = 25909, type = "HELPFUL", assignto = "totemCluster3", isMine = true, color = {149/255, 121/255, 214/255} }  -- Tranquil Air Totem
-    A{ id = { 8836, 10626, 25360 }, type = "HELPFUL", assignto = "totemCluster3", isMine = true, color = { 65/255, 110/255, 1 } }  -- Grace of Air Totem
-    A{ id = { 10596, 10598, 10599 }, type = "HELPFUL", assignto = "totemCluster3", isMine = true, color = {52/255, 172/255, 114/255} }  -- Nature Resistance Totem
+    A{ id = 8178, type = "HELPFUL", assignto = "raidbuff", priority = prioAir, isMine = true, color = { 0.6, 0, 1 } }  -- Grounding Totem
+    A{ id = 25909, type = "HELPFUL", assignto = "raidbuff", priority = prioAir, isMine = true, color = {149/255, 121/255, 214/255} }  -- Tranquil Air Totem
+    A{ id = { 8836, 10626, 25360 }, type = "HELPFUL", assignto = "raidbuff", priority = prioAir, isMine = true, color = { 65/255, 110/255, 1 } }  -- Grace of Air Totem
+    A{ id = { 10596, 10598, 10599 }, type = "HELPFUL", assignto = "raidbuff", priority = prioAir, isMine = true, color = {52/255, 172/255, 114/255} }  -- Nature Resistance Totem
 
     -- Ancestral Healing
     A{ id = { 16177, 16236, 16237 }, type = "HELPFUL", assignto = "bars", showDuration = true, color = { 1, 0.85, 0} }
