98f46db6a45bda0cdfdc7b557cf5106cd59e1b1a
d87 <d87d87@gmail.com>
2020-09-01 09:12:21 +0700

    Switched debuff hightlighting to border

--- config.lua
+++ config.lua
@@ -58,7 +58,7 @@ config.HealthBarColor = { name = "HealthBar", assignto = "health", color = {1, .
 config.PowerBarColor = { name = "PowerBar", assignto = "power", color = {.5,.5,1}, priority = 20 }
 config.InVehicleStatus = { name = "InVehicle", assignto = "vehicle", color = {0.3,1,0.3}, priority = 21 }
 config.LOSStatus = { name = "OutOfSight", assignto = "healfeedback", scale = 1.6, color = {1,0.1,0.1}, priority = 95, fade = 0.3 }
-config.DispelStatus = { name = "Dispel", assignto = "bossdebuff", scale = 0.8, priority = 20 }
+config.DispelStatus = { name = "Dispel", assignto = "border", scale = 0.8, priority = 20 }
 config.StaggerStatus = { name = "Stagger", assignto = "text2", priority = 20 }
 config.RunicPowerStatus = { name = "RunicPower", assignto = "mitigation", priority = 10, color = { 0, 0.82, 1 } }
 
@@ -93,13 +93,10 @@ if isClassic then
     config.DefaultWidgets.totemCluster2 = { type = "Indicator", width = 5, height = 5, point = "TOPLEFT", x = pixelperfect(12), y = 0 }
     config.DefaultWidgets.totemCluster3 = { type = "Indicator", width = 5, height = 5, point = "TOPLEFT", x = pixelperfect(19), y = 0 }
 end
--- for name,w in pairs(config.DefaultWidgets) do
---     w.__protected = true
--- end
 
 config.BossDebuffs = {
-    { name = "BossDebuffLevel1", assignto = "bossdebuff", color = {1,0,0}, priority = 95, pulse = true, },
-    { name = "BossDebuffLevel2", assignto = "bossdebuff", color = {1,0,1}, priority = 95, pulse = true, },
+    { name = "BossDebuffLevel1", assignto = { "border", "flash" }, color = {1,0,0}, priority = 95, pulse = true, },
+    { name = "BossDebuffLevel2", assignto = { "border", "flash" }, color = {1,0,1}, priority = 95, pulse = true, },
     { name = "BossDebuffLevel3", assignto = { "innerglow", "border", "flash" }, color = {1,0,0}, priority = 90 },
     { name = "BossDebuffLevel4", assignto = "pixelGlow", color = {1,1,1}, priority = 95 },
     -- { name = "BossDebuffLevel3", assignto = "autocastGlow", color = {1,1,0.3}, priority = 90 },
