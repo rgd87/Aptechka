ea02c56e35be854678e460761e118114ec938673
d87 <d87d87@gmail.com>
2024-09-28 20:25:11 +0700

    Added SoD Water spell ID

--- config.classic.lua
+++ config.classic.lua
@@ -79,6 +79,7 @@ AG{ id = { 20707, 20762, 20763, 20764, 20765 }, type = "HELPFUL", global = true,
 
 AG{ id = {
     430, 431, 432, 1133, 1135, 1137, 22734, 24355, 29007, 26473, 26261, -- Classic water
+    468767, -- SoD
 }, assignto = set("text2"), color = {0.7, 0.7, 1}, text = "DRINKING", global = true, priority = 30 }
 
 -- Stealth, Prowl, Shadowmeld
